01ff4551ced4c91ffae927e3e27a346887f06955
TheBookSloth <JacobLevine07@gmail.com>
2026-09-09 14:21:29 -0400

    Added Vertical Player Movement Limits

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //traveling up
-        if (Input.GetKey(KeyCode.W))
+        //traveling up or down
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(speed * Time.deltaTime * transform.up);
         }
-        else if (Input.GetKey(KeyCode.S)) 
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) 
         {
             transform.Translate(speed * Time.deltaTime * -transform.up);
         }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4.5f, 4.5f), 0);
     }
 }
